c5db69a4c9beda86f3b2246becc30b963f91aed9
mwanikiiiii <robertkaruga12@gmail.com>
2022-10-04 04:49:29 -0700

    dd

--- 0x1E-search_algorithms/100-jump.c
+++ 0x1E-search_algorithms/100-jump.c
@@ -1,55 +1,41 @@
 #include "search_algos.h"
-#include <math.h>
+
+#define PRINT_CHECKED(idx, item) \
+	printf("Value checked array[%lu] = [%d]\n", (idx), (item))
+
+#define PRINT_BOUNDED(low, high) \
+	printf("Value found between indexes [%lu] and [%lu]\n", (low), (high))
 
 /**
- * jump_search - searches for a value in a sorted array of integers
- * using the Jump search algorithm
- * @array: array to look into
- * @size: size of the array
- * @value: value to look for
+ * jump_search - search for a value in a sorted array of integers
+ * @array: the array of values
+ * @size: the number of values
+ * @value: the value to locate
  *
- * Return: index of the value found, or -1
+ * Return: If value is not present in array or array is NULL, return -1.
+ * Otherwise, returh the first index where value is located.
  */
 int jump_search(int *array, size_t size, int value)
 {
-	size_t start, end, step;
+	size_t i = 0, j = 0;
 
-	if (!array || size == 0)
-		return (-1);
-	step = sqrt(size);
-	start = 0, end = step;
-
-	while (start < size)
+	if (array && size)
 	{
-		printf("Value checked array[%lu] = [%d]\n", start, array[start]);
-		if (end < size)
+		while (j < size && array[j] < value)
 		{
-			if (array[start] <= value && value <= array[end])
-			{
-				printf("Value found between indexes [%lu] and [%lu]\n", start, end);
-				break;
-			}
-		} else
-		{
-			if (array[start] <= value)
-			{
-				printf("Value found between indexes [%lu] and [%lu]\n", start, end);
-				break;
-			}
+			PRINT_CHECKED(j, array[j]);
+			i = j, j += 3;
 		}
-		start = end;
-		end = start + step;
+		PRINT_BOUNDED(i, j);
+		if (size <= j)
+			j = size - 1;
+		while (i <= j)
+		{
+			PRINT_CHECKED(i, array[i]);
+			if (array[i] == value)
+				return (i);
+			++i;
+		}
 	}
-
-	while (start <= end)
-	{
-		if (start == size)
-			return (-1);
-		printf("Value checked array[%lu] = [%d]\n", start, array[start]);
-		if (array[start] == value)
-			return (start);
-		start++;
-	}
-
 	return (-1);
 }
